ffba1b53d68efa0eaf261b4fac8837403a77f149
mostofameem <mostofameem@gmail.com>
2024-05-20 17:28:41 +0600

    JWT

--- cmd/main.go
+++ cmd/main.go
@@ -12,10 +12,10 @@ func main() {
 	if err := db.InitDB(); err != nil {
 		log.Fatal("Error initializing database:", err)
 	}
-	//defer db.Db.Close()
+	defer db.Db.Close()
 
 	mux := web.StartServer()
-	log.Printf("Server Running on port 30000")
+	log.Printf("Server Running on port 3000")
 	log.Fatal(http.ListenAndServe(":3000", mux))
 
 }
